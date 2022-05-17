7b04630f5d848075cdc0553d3f25778cbc4e515b
kazut98 <87102743+kazut98@users.noreply.github.com>
2022-05-17 04:06:47 -0300

    Add files via upload

--- Cafe_DataAnalysis.R
+++ Cafe_DataAnalysis.R
@@ -2,8 +2,31 @@
 
 install.packages("readxl")
 library("readxl")
+install.packages("ggplot2")
+library("ggplot2")
+install.packages("tidyverse")
+library("tidyverse")
 
-#Data
+?dplyr
+
+#Data 
 
 df_cafe <- read_excel("C:/Users/Kazuhiro/OneDrive/Documentos/Data_Cafe.xlsx")
+
+
+#from tibble to dataframe
+df_cafe <- as.data.frame(df_cafe)
+head(df_cafe)
 str(df_cafe)
+
+#Remove 1st row 
+df_cafe <- df_cafe[-1]
+str(df_cafe)
+
+
+#remove columns: total de trabalhadores, homens, %homens, mulheres, %mulheres
+
+#feature engineering 
+
+
+ 
