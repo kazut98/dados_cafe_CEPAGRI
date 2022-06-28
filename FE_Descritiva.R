9222f204117113d640418671234fc398649c3f28
kazut98 <87102743+kazut98@users.noreply.github.com>
2022-06-28 17:40:02 -0300

    Update FE_Descritiva.R

--- FE_Descritiva.R
+++ FE_Descritiva.R
@@ -3,17 +3,24 @@ library("data.table")
 library("dplyr")
 setwd('C:/Users/ra236134/Downloads')
 dados_cafe <- read_excel('C:/Users/ra236134/Downloads/Data_Coffee.xlsx', skip=1)
-head(dados_cafe)
+
+#################################################################################################
+
+#creating tabbles
 
 tabela_2017 <- dados_cafe[, 1:46] 
 tabela_2006 <- dados_cafe[, 48:87]
 tabela_95_96 <- dados_cafe[, 89:101]
 
-dados_2017 <- tabela_2017 %>% select (-c(Homens...35, Mulheres...37, `Porcentagem deHomens...36`,
-                                         `Porcentagem deMulheres...38`, `Total de trabalhadores...34`))
+dados_2017 <- tabela_2017 %>% select (-c(Homens...35, Mulheres...37, 
+                                         `Porcentagem deHomens...36`,
+                                         `Porcentagem deMulheres...38`, 
+                                         `Total de trabalhadores...34`))
 
-dados_2006 <- tabela_2006 %>% select (-c(Homens...76, Mulheres...78, `Porcentagem deHomens...77`, 
-                                         `Porcentagem deMulheres...79`, `Total de trabalhadores...75`))
+dados_2006 <- tabela_2006 %>% select (-c(Homens...76, Mulheres...78,
+                                         `Porcentagem deHomens...77`, 
+                                         `Porcentagem deMulheres...79`,
+                                         `Total de trabalhadores...75`))
 
 dados_95_96 <- tabela_95_96 %>% select (-c(Homens...97, Mulheres...99, `Porcentagem dehomens`, 
                                            `Porcentagem demulheres`))
@@ -22,8 +29,9 @@ names(dados_2017) <- sub("\\....*", "", names(dados_2017))
 names(dados_2006) <- sub("\\....*", "", names(dados_2006))
 names(dados_95_96) <- sub("\\....*", "", names(dados_95_96))
 
-#------#
+#################################################################################################
 
+#descriptive statistics
 media_2017 <- sapply(dados_2017[2:41], mean)
 dp_2017 <- sapply(dados_2017[2:41], sd)
 min_2017 <- sapply(dados_2017[2:41], min)
